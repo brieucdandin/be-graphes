edabd075e3b17195f18e7160634e26059545e0e0
raphinsa <raphael.p@bbox.fr>
2018-05-27 11:37:32 +0200

    Dijkstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -37,7 +37,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     
     
     @Override
-    protected ShortestPathSolution doRun() {
+    protected ShortestPathSolution doRun() 
+    {
         ShortestPathData data = getInputData();
 
         /**         INITIALISATION
@@ -54,7 +55,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         //Creation d'un tableau Label pour marquer les Labels'
         Label[] marquage = new Label[data.getGraph().size()];
 
-        for(Node n: data.getGraph()) {
+        for(Node n: data.getGraph()) 
+        {
             marquage[n.getId()] = new Label(n);
         }
         marquage[data.getOrigin().getId()].setCout(0.0);
@@ -88,9 +90,11 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
         boolean val = true;
         int nbNoeud = 0;
-        while (val) {
+        while (val) 
+        {
             val = false;
-            if(!tas.isEmpty()){
+            if(!tas.isEmpty())
+            {
                 Label lx = tas.deleteMin();
                 Node x = lx.getNoeud();
                 notifyNodeMarked(x);
@@ -101,7 +105,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
                 //On parcourt la liste des successeurs de x (obtenue ci-dessus).
                 for (Arc succ : x)
                 {
-                	if(!data.isAllowed(succ)){
+                	if(!data.isAllowed(succ))
+                	{
                 		continue;
                 	}
                 	Label ly = marquage[succ.getDestination().getId()];
@@ -111,43 +116,50 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
                 		double a = ly.getCout();
                 		double cout = Math.min(ly.getCout(), lx.getCout()+data.getCost(succ));
                 		ly.setCout(cout);
-                		if(cout != a){
+                		if(cout != a)
+                		{
                 			tas.insert(ly);
                 			prec[succ.getDestination().getId()] = succ;
                 		} 
-                		if (Double.isInfinite(a) && Double.isFinite(cout)){
+                		if (Double.isInfinite(a) && Double.isFinite(cout))
+                		{
                 			notifyNodeReached(ly.getNoeud());
                 		}
                 	}
                 }
 
-                if ((x.equals(data.getDestination())) || (nbNoeud >= data.getGraph().size())) {
+                if ((x.equals(data.getDestination())) || (nbNoeud >= data.getGraph().size())) 
+                {
                 	val = false;
                 }
-                else{
+                else
+                {
                 	val = true;
                 }
             }
-
-            ShortestPathSolution solution = null;
-
-	        //la destination n'a pas de noeud precedent alors on ne peut pas utiliser l'algorithme
-	        if (prec[data.getDestination().getId()]==null){
-	            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
-	        }
-	        else{
-	            notifyDestinationReached(data.getDestination());
-	
-	            ArrayList <Arc> ListArc = new ArrayList<>();
-	            Arc arc = prec[data.getDestination().getId()];
-	            while(arc != null){
-	                ListArc.add(arc);
-	                arc = prec[arc.getOrigin().getId()];
-	            }
-	            Collections.reverse(ListArc);
-	            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(data.getGraph(), ListArc));
-	        }
-	        return solution;
         }
+
+        ShortestPathSolution solution = null;
+
+	    //la destination n'a pas de noeud precedent alors on ne peut pas utiliser l'algorithme
+	    if (prec[data.getDestination().getId()]==null)
+	    {
+	    	solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+	    }
+	    else
+	    {
+	    	notifyDestinationReached(data.getDestination());
+	
+	        ArrayList <Arc> ListArc = new ArrayList<>();
+	        Arc arc = prec[data.getDestination().getId()];
+	        while(arc != null)
+	        {
+	        	ListArc.add(arc);
+	            arc = prec[arc.getOrigin().getId()];
+	        }
+	        Collections.reverse(ListArc);
+	        solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(data.getGraph(), ListArc));
+	    }
+	    return solution;
     }
 }
